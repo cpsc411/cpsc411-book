9602130f7e899af3b934cb9fe4d0ad4c8d598e1b
William J. Bowman <wjb@williamjbowman.com>
2021-01-28 16:50:25 -0800

    Assume everything is one week, for now

--- config.rkt
+++ config.rkt
@@ -17,7 +17,7 @@
     (hash-set! x 'a1 (+weeks (hash-ref x 'a0) 1))
     (hash-set! x 'a2 (+weeks (hash-ref x 'a1) 2))
     (hash-set! x 'a3 (+weeks (hash-ref x 'a2) 1))
-    (hash-set! x 'a4 (+weeks (hash-ref x 'a3) 2))
+    (hash-set! x 'a4 (+weeks (hash-ref x 'a3) 1))
     (hash-set! x 'a5 (+weeks (hash-ref x 'a4) 1))
     (hash-set! x 'a6 (+weeks (hash-ref x 'a5) 1))
     (hash-set! x 'a7 (+weeks (hash-ref x 'a6) 1))
