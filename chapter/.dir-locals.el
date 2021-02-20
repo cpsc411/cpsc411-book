93ca10481702861c55ee0ab0aa730bc03c1776e5
William J. Bowman <wjb@williamjbowman.com>
2021-02-19 16:49:08 -0800

    Move spell check config to dir-locals

--- chapter/.dir-locals.el
+++ chapter/.dir-locals.el
@@ -3,5 +3,14 @@
 
 ((emacs-lisp-mode . ((eval . (put 'for/foldr 'racket-indent-function 2))
                      (eval . (put 'module 'racket-indent-function 0))
-                     (eval . (put 'return-point 'racket-indent-function 1))))
- (scribble-mode . ((eval . (put 'nd-match 'racket-indent-function 1)))))
+                     (eval . (put 'return-point 'racket-indent-function 1))
+                     (eval . (put 'nd-match 'racket-indent-function 1))))
+ (nil (ispell-buffer-session-localwords
+       "lang" "rkt" "TODO" "eg" "eval" "behaviour" "pm" "url" "todo"
+       "subsubsub" "tt" "ids" "GitHub" "secref" "emph" "itemlist" "primops"
+       "uniquify" "Rice's" "Exprs" "eq" "racketgrammar" "fixnum" "uint"
+       "ascii" "binop" "unop" "deftech" "primop" "int" "racketblock" "lib"
+       "nasm" "Dybvig" "Chez" "ptr" "fixnums" "immediates" "xor" "ptrs"
+       "Paren" "sar" "loc" "ie" "triv" "reg" "trg" "opand" "cmp" "addr" "fbp"
+       "bnf" "ior" "dispoffset" "unsyntax" "neq" "binops" "aloc" "rloc" "fvar"
+       "fvars" "pre" "CPS" "undead" "fx" "codeblock" "falsey" "unops" "eval")))
