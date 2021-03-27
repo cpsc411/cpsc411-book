6fa507165fa14ad3c0b7fd216313e20906af7997
William J. Bowman <wjb@williamjbowman.com>
2021-03-27 15:59:05 -0700

    Fix error in .dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,8 +1,22 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((emacs-lisp-mode . ((eval . (put 'for/foldr 'racket-indent-function 2))
-                     (eval . (put 'module 'racket-indent-function 0))
-                     (eval . (put 'return-point 'racket-indent-function 1))
-                     (eval . (put 'nd-match 'racket-indent-function 1))))
- (nil (ispell-buffer-session-localwords "lang" "rkt" "TODO" "eg" "eval" "behaviour" "pm" "url" "todo" "subsubsub" "tt" "ids" "GitHub" "secref" "emph" "itemlist" "primops" "uniquify" "Rice's" "Exprs" "eq" "racketgrammar" "fixnum" "uint" "ascii" "binop" "unop" "deftech" "primop" "int" "racketblock" "lib" "nasm" "Dybvig" "Chez" "ptr" "fixnums" "immediates" "xor" "ptrs" "Paren" "sar" "loc" "ie" "triv" "reg" "trg" "opand" "cmp" "addr" "fbp" "bnf" "ior" "dispoffset" "unsyntax" "neq" "binops" "aloc" "rloc" "fvar" "fvars" "pre" "CPS" "undead" "fx" "codeblock" "falsey" "unops" "NB" "abi" "asm" "rbp" "fv" "tmp" "rp" "opands" "ra" "fb" "fp" "rv" "nfv" "rax" "ctx" "rx" "rsp" "rbx" "rcx" "rdx" "rsi" "rdi" "regexp" "alocs" "ust" "rlocs" "andmap" "listof" "datatype" "interp" "factn" "nb" "jmp" "unspillable" "unspillables" "nop" "ss" "foldr" "NASM" "mov"))
+((racket-mode . ((eval . (put 'for/foldr 'racket-indent-function 2))
+                 (eval . (put 'module 'racket-indent-function 0))
+                 (eval . (put 'return-point 'racket-indent-function 1))
+                 (eval . (put 'nd-match 'racket-indent-function 1))))
+ (nil . (ispell-buffer-session-localwords
+         "lang" "rkt" "TODO" "eg" "eval"
+         "behaviour" "pm" "url" "todo" "subsubsub" "tt" "ids" "GitHub"
+         "secref" "emph" "itemlist" "primops" "uniquify" "Rice's"
+         "Exprs" "eq" "racketgrammar" "fixnum" "uint" "ascii" "binop"
+         "unop" "deftech" "primop" "int" "racketblock" "lib" "nasm"
+         "Dybvig" "Chez" "ptr" "fixnums" "immediates" "xor" "ptrs"
+         "Paren" "sar" "loc" "ie" "triv" "reg" "trg" "opand" "cmp"
+         "addr" "fbp" "bnf" "ior" "dispoffset" "unsyntax" "neq" "binops"
+         "aloc" "rloc" "fvar" "fvars" "pre" "CPS" "undead" "fx"
+         "codeblock" "falsey" "unops" "NB" "abi" "asm" "rbp" "fv" "tmp"
+         "rp" "opands" "ra" "fb" "fp" "rv" "nfv" "rax" "ctx" "rx" "rsp"
+         "rbx" "rcx" "rdx" "rsi" "rdi" "regexp" "alocs" "ust" "rlocs"
+         "andmap" "listof" "datatype" "interp" "factn" "nb" "jmp"
+         "unspillable" "unspillables" "nop" "ss" "foldr" "NASM" "mov")))
