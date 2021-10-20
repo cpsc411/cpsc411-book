3308ca8924512fb518d21912e3570d033d5ae969
William J. Bowman <wjb@williamjbowman.com>
2021-10-19 20:13:48 -0700

    Fix dependency issue

--- info.rkt
+++ info.rkt
@@ -3,6 +3,11 @@
 (define deps '("at-exp-lib"
                "cpsc411-lib"
                "gregor-lib"
+               ;; NOTE: memoize is required by a dependency of gregor lib, so force install my forked version. Otherwise, we get package conflicts.
+               "https://github.com/wilbowma/memoize?path=memoize-lib"
+               "https://github.com/wilbowma/memoize?path=memoize-doc"
+               "https://github.com/wilbowma/memoize?path=memoize-test"
+               "https://github.com/wilbowma/memoize?path=memoize"
                "racket-graphviz"
                ("scribble-bettergrammar-lib" #:version "1.4.2")
                "scribble-lib"
