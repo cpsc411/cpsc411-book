c5ac3e20bc00ed331ca031e0b488e5670af457bc
William J. Bowman <wjb@williamjbowman.com>
2021-10-23 17:23:54 -0700

    Fix github dependency schema

--- info.rkt
+++ info.rkt
@@ -4,10 +4,10 @@
                "cpsc411-lib"
                "gregor-lib"
                ;; NOTE: memoize is required by a dependency of gregor lib, so force install my forked version. Otherwise, we get package conflicts.
-               "https://github.com/wilbowma/memoize?path=memoize-lib"
-               "https://github.com/wilbowma/memoize?path=memoize-doc"
-               "https://github.com/wilbowma/memoize?path=memoize-test"
-               "https://github.com/wilbowma/memoize?path=memoize"
+               "https://github.com/wilbowma/memoize.git?path=memoize-lib"
+               "https://github.com/wilbowma/memoize.git?path=memoize-doc"
+               "https://github.com/wilbowma/memoize.git?path=memoize-test"
+               "https://github.com/wilbowma/memoize.git?path=memoize"
                "racket-graphviz"
                ("scribble-bettergrammar-lib" #:version "1.4.2")
                "scribble-lib"
