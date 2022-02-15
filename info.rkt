35e4e74b68aa2e640e4f85f63f7297e80d21ae4e
William J. Bowman <wjb@williamjbowman.com>
2022-02-14 22:59:39 -0800

    Bump requires better-grammar version
    Remove customization in favour of newest better-grammar styling.

--- info.rkt
+++ info.rkt
@@ -6,7 +6,7 @@
                "gregor-lib"
                "memoize-lib"
                "racket-graphviz"
-               ("scribble-bettergrammar-lib" #:version "1.4.2")
+               ("scribble-bettergrammar-lib" #:version "1.6.0")
                "scribble-lib"
                "web-server-lib"
                "with-cache"
@@ -28,7 +28,7 @@
     ;"cpsc411-reference-lib"
     "https://github.com/cpsc411/cpsc411-priv.git?path=cpsc411-reference-lib#2021w2"
     "racket-graphviz"
-    ("scribble-bettergrammar-lib" #:version "1.4.2")))
+    ("scribble-bettergrammar-lib" #:version "1.6.0")))
 (define scribblings '(("index.scrbl" (multi-page no-search))))
 (define test-omit-paths (list "serve.rkt"))
 (define pkg-desc "The CPSC411 Book, which includes lecture notes and assignments.")
