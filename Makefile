95cb20f69a5122103f933dc8511ea06b61e016b8
William J. Bowman <wjb@williamjbowman.com>
2022-09-06 13:04:05 -0700

    Update term strings and dates to 2022w2

--- Makefile
+++ Makefile
@@ -30,7 +30,7 @@ clean:
 	rm -rf compiled/
 
 sync: build/*.html build/*.cgi
-	rsync -avzl --delete --delete-excluded --chmod=Dg+x,g+r build/ cs-411@remote.students.cs.ubc.ca:public_html/2021w2/
+	rsync -avzl --delete --delete-excluded --chmod=Dg+x,g+r build/ cs-411@remote.students.cs.ubc.ca:public_html/2022w2/
 
 serve: build/index.html
 	racket -t serve.rkt
--- config.rkt
+++ config.rkt
@@ -7,8 +7,8 @@
 (provide (all-defined-out))
 
 (define course-number "CPSC 411")
-(define semester "2021w2")
-(define gh-org "cpsc411-2021w-t2")
+(define semester "2022w2")
+(define gh-org "cpsc411-2022w-t2")
 (define course-title "Introduction to Compiler Construction")
 
 (define url-root (string-append "https://www.students.cs.ubc.ca/~cs-411/" semester))
@@ -16,13 +16,12 @@
 ;; TODO: Are you serious? Racket can't handle a recursive value?!
 (define deadline-dict
   (let ([x (make-hasheq)])
-    ;; First Friday of Term
-    (hash-set! x 'a0 (moment 2022 1 14 23 59 59))
-    (hash-set! x 'a1 (+weeks (hash-ref x 'a0) 1))
+    ;; First Sunday of Term
+    (hash-set! x 'a0 (moment 2023 1 15 23 59 59))
+    (hash-set! x 'a1 (+weeks (hash-ref x 'a0) 2))
     (hash-set! x 'a2 (+weeks (hash-ref x 'a1) 1))
     (hash-set! x 'a3 (+weeks (hash-ref x 'a2) 1))
-    ;; Change from Friday to Sunday due dates by popular request
-    (hash-set! x 'a4 (+days (+weeks (hash-ref x 'a3) 2) 2))
+    (hash-set! x 'a4 (+weeks (hash-ref x 'a3) 2))
     (hash-set! x 'a5 (+weeks (hash-ref x 'a4) 1))
     (hash-set! x 'a6 (+weeks (hash-ref x 'a5) 2))
     (hash-set! x 'a7 (+weeks (hash-ref x 'a6) 1))
@@ -56,13 +55,13 @@
     (,(-days (hash-ref deadline-dict 'a10) 4)  . "Weekly Readings: Chp 12 (5.12)")
     #;(,(-days (hash-ref deadline-dict 'a11) 4)  . "Weekly Readings: Chp 13 (5.13)")
     #;(,(moment 2021 2 22) . "Weekly Readings: Chp 7 (5.7)")
-    (,(moment 2022 2 28) . ,(emph "Midterm! (maybe)"))
+    #;(,(moment 2022 2 28) . ,(emph "Midterm! (maybe)"))
 
-    (,(moment 2022 1 11) . "First Day of Lecture")
-    (,(moment 2022 1 18) . "No lecture; out at POPL")
-    (,(moment 2022 1 20) . "No lecture; out at POPL")
-    (,(moment 2022 1 21) . "Last day to withdraw, without W")
-    (,(moment 2022 3 4) . "Last day to withdraw, with W")
-    (,(moment 2022 2 21) . "Start of Midterm Break")
-    (,(moment 2022 2 25) . "End of Midterm Break")
-    (,(moment 2022 4 8) . "Last Day of Lecture")))
+    (,(moment 2023 1 10) . "First Day of Lecture")
+    (,(moment 2023 1 17) . "Maybe no lecture; out at POPL")
+    (,(moment 2023 1 19) . "Maybe no lecture; out at POPL")
+    (,(moment 2023 1 20) . "Last day to withdraw, without W")
+    (,(moment 2023 3 3) . "Last day to withdraw, with W")
+    (,(moment 2023 2 20) . "Start of Midterm Break")
+    (,(moment 2023 2 24) . "End of Midterm Break")
+    (,(moment 2023 4 13) . "Last Day of Lecture")))
